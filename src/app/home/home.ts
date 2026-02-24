89542da7075cdfee82487b28610e509425c59545
Josh Gronneberg <joshua.gronneberg@ndsu.edu>
2026-02-23 22:57:29 -0600

    Added the search feature to the angular app

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -13,9 +13,17 @@ export class Home {
   // readonly baseUrl = "https://angular.dev/assets/images/tutorials/common";
   housingLocationList: Housinglocationinfo[] = [];
   housingService: HousingService = inject(HousingService);
+  filteredLocationList: Housinglocationinfo[] = [];
   constructor(){
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
   }
-
+  filterResults(text: string) {
+    if(!text){
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+    this.filteredLocationList = this.housingLocationList.filter((housingLocation) => housingLocation?.city.toLowerCase().includes(text.toLowerCase()), );
+  }
   
 }
